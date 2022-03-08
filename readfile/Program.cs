00976ea93f99b3d59be0be68c5a0096c02bdb19a
faizmaredia <84702138+faizmaredia@users.noreply.github.com>
2022-03-08 13:51:55 +0530

    Add files via upload

---
+++ readfile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader("E:\\dotNet\\dotNetPractical_5\\Sample.txt");
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
---
+++ writefile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace writefile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                StreamWriter sw = new StreamWriter("E:\\dotNet\\dotNetPractical_5\\Sample.txt");
+
+                sw.WriteLine("Hello Guys !!!");
+                sw.WriteLine("Hello Guys !!! 2nd Line");
+
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
